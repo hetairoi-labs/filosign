fc8f1a07cd728b520bfe46f8e838fe252b31efa6
Spandan Barve <spandan567@gmail.com>
2026-03-29 09:23:30 +0530

    added escrow functionality to IFSManager interface

--- packages/contracts/src/interfaces/IFSManager.sol
+++ packages/contracts/src/interfaces/IFSManager.sol
@@ -8,14 +8,20 @@ interface IFSManager {
     function fileRegistry() external view returns (address);
     function keyRegistry() external view returns (address);
     function worldVerifier() external view returns (address);
+    function escrow() external view returns (address);
     function server() external view returns (address);
     function version() external view returns (uint8);
     function approvedSenders(address key, address key1) external view returns (bool);
     event SenderApproved();
     event SenderRevoked();
+    event IncentiveAttached();
+    event IncentivesReleased();
     function setActiveVersion(uint8 version_) external;
     function setWorldVerifier(address _worldVerifier) external;
     function isRegistered(address account_) external view returns (bool);
     function approveSender(address sender_) external;
     function revokeSender(address sender_) external;
+    function attachIncentive(string calldata pieceCid_, address signer_, address token_, uint256 amount_) external;
+    function attachIncentiveWithPermit(string calldata pieceCid_, address signer_, address token_, uint256 amount_, uint256 deadline_, uint8 v_, bytes32 r_, bytes32 s_) external;
+    function releaseIncentives(string calldata pieceCid_, address[] calldata signers_) external;
 }
