d29da4b884bdb03e77e0ed4b16cc2857e82c7cff
Styles <kartik100100@gmail.com>
2026-05-09 14:57:11 +0530

    refactor: update breadcrumb and carousel components

--- packages/client/src/lib/components/ui/carousel.tsx
+++ packages/client/src/lib/components/ui/carousel.tsx
@@ -3,9 +3,8 @@ import useEmblaCarousel, {
 	type UseEmblaCarouselType,
 } from "embla-carousel-react";
 import * as React from "react";
-
-import { cn } from "../../utils";
-import { Button } from "./button";
+import { Button } from "@/src/lib/components/ui/button";
+import { cn } from "@/src/lib/utils/index";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
@@ -172,7 +171,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
 function CarouselPrevious({
 	className,
 	variant = "outline",
-	size = "icon",
+	size = "icon-sm",
 	...props
 }: React.ComponentProps<typeof Button>) {
 	const { orientation, scrollPrev, canScrollPrev } = useCarousel();
@@ -183,7 +182,7 @@ function CarouselPrevious({
 			variant={variant}
 			size={size}
 			className={cn(
-				"absolute size-8 rounded-full",
+				"absolute touch-manipulation rounded-full",
 				orientation === "horizontal"
 					? "top-1/2 -left-12 -translate-y-1/2"
 					: "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -202,7 +201,7 @@ function CarouselPrevious({
 function CarouselNext({
 	className,
 	variant = "outline",
-	size = "icon",
+	size = "icon-sm",
 	...props
 }: React.ComponentProps<typeof Button>) {
 	const { orientation, scrollNext, canScrollNext } = useCarousel();
@@ -213,7 +212,7 @@ function CarouselNext({
 			variant={variant}
 			size={size}
 			className={cn(
-				"absolute size-8 rounded-full",
+				"absolute touch-manipulation rounded-full",
 				orientation === "horizontal"
 					? "top-1/2 -right-12 -translate-y-1/2"
 					: "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -236,4 +235,5 @@ export {
 	CarouselItem,
 	CarouselPrevious,
 	CarouselNext,
+	useCarousel,
 };
