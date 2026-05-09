7ba7d0a44d88b11a3bfd6b9d589efa1cc9c64aff
Styles <kartik100100@gmail.com>
2026-05-09 14:57:23 +0530

    refactor: update collapsible component

--- packages/client/src/lib/components/ui/collapsible.tsx
+++ packages/client/src/lib/components/ui/collapsible.tsx
@@ -1,36 +1,18 @@
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
-import { cn } from "../../utils";
+import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible";
 
-function Collapsible({
-	...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
 	return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />;
 }
 
-function CollapsibleTrigger({
-	...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+function CollapsibleTrigger({ ...props }: CollapsiblePrimitive.Trigger.Props) {
 	return (
-		<CollapsiblePrimitive.CollapsibleTrigger
-			data-slot="collapsible-trigger"
-			{...props}
-		/>
+		<CollapsiblePrimitive.Trigger data-slot="collapsible-trigger" {...props} />
 	);
 }
 
-function CollapsibleContent({
-	className,
-	...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+function CollapsibleContent({ ...props }: CollapsiblePrimitive.Panel.Props) {
 	return (
-		<CollapsiblePrimitive.CollapsibleContent
-			data-slot="collapsible-content"
-			className={cn(
-				"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:slide-out-to-top-1 data-[state=open]:slide-in-from-top-1 transition-all duration-200",
-				className,
-			)}
-			{...props}
-		/>
+		<CollapsiblePrimitive.Panel data-slot="collapsible-content" {...props} />
 	);
 }
 
