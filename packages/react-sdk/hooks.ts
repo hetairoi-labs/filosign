011a75c6aafcf176d6f0f6873278735130c9278d
Styles <kartik100100@gmail.com>
2026-05-16 10:59:30 +0530

    [REACT SDK] - Domain export map (packages/react-sdk): replace monolithic hooks barrel with auth/files/sharing/users/runtime.

--- packages/react-sdk/hooks.ts
+++
@@ -1,8 +0,0 @@
-export * from "./src/hooks/auth";
-// export * from "./src/hooks/core";
-export * from "./src/hooks/files";
-export * from "./src/hooks/sharing";
-export * from "./src/hooks/useRuntimeChain";
-export * from "./src/hooks/users";
-
-// testing if cloudflare build paths works
