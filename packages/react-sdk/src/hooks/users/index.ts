921a3e4d8e6522fc5e2a6cbf1616e5d74197be30
Styles <kartik100100@gmail.com>
2026-05-16 10:59:46 +0530

    [REACT SDK] - Users barrel cleanup (packages/react-sdk): remove unused useUpdateUserProfilePrevalidate export and hook.

--- packages/react-sdk/src/hooks/users/index.ts
+++ packages/react-sdk/src/hooks/users/index.ts
@@ -2,7 +2,6 @@ export { useProfilesByAddresses } from "./useProfilesByAddresses";
 export { useSetPrimaryEmail } from "./useSetPrimaryEmail";
 export { useSyncPrivyEmail } from "./useSyncPrivyEmail";
 export { useUpdateUserProfile } from "./useUpdateUserProfile";
-export { useUpdateUserProfilePrevalidate } from "./useUpdateUserProfilePrevalidate";
 export {
 	fetchUserProfile,
 	type UserProfile,
